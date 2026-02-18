498a5e29f17c2dc1019fcfccaf500ca6b17b7896
Benjamin Otto <git@otto.bayern>
2026-02-18 20:02:11 +0100

    Prep dev

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from monkeycamsp!")
+# [A1] Regionen im Video auswählen für Zeitstempel und Türerkennung
+
+# [A2] Ausschnitt für die Türerkennung vorbereiten
+
+# [A3] Videos aus Ordner laden und verarbeiten
+
+# [A4] Für jede Türerkennung Screenshot erstellen und Zeitstempel speichern
+
+# [B1] UI für die Zuweisung vorbereiten
+
+# [B2] Bilder aus Ordner laden
+
+# [B4] Zuweisung durchführen
+
+# [B5] Ergebnisse in CSV speichern
 
 
 if __name__ == "__main__":
-    main()
+	pass
